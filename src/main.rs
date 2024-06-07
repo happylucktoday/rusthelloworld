92cc13b7da7ec648238ac561447c290b8e09ae5e
happylucktoday <happyluck202401@outlook.com>
2024-06-07 07:16:12 -0500

    updated

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use ferris_says::say;
+
+use std::io::{stdout, BufWriter};
+
 fn main() {
-    println!("Hello, world!");
+    let stdout = stdout();
+    let message = String::from("Hello, fello Rustaceans!");
+    let width = message.chars().count();
+
+    let mut writer = BufWriter::new(stdout.lock());
+    say(&message, width, &mut writer).unwrap();
 }
